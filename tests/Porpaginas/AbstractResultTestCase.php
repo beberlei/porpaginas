4565f10537d9e8160a4056ccb13703e240bd558f
Benjamin Eberlei <kontakt@beberlei.de>
2014-01-04 15:51:56 +0100

    Finish tests for Page object.

--- tests/Porpaginas/AbstractResultTestCase.php
+++ tests/Porpaginas/AbstractResultTestCase.php
@@ -24,5 +24,36 @@ abstract class AbstractResultTestCase extends \PHPUnit_Framework_TestCase
         $this->assertCount(11, iterator_to_array($result));
     }
 
+    /**
+     * @test
+     */
+    public function it_takes_slice_as_page()
+    {
+        $result = $this->createResultWithItems(11);
+
+        $page = $result->take(0, 10);
+
+        $this->assertEquals(1, $page->getCurrentPage());
+        $this->assertEquals(0, $page->getCurrentOffset());
+        $this->assertEquals(10, $page->getCurrentLimit());
+        $this->assertCount(10, $page);
+        $this->assertEquals(11, $page->totalCount());
+    }
+
+    /**
+     * @test
+     */
+    public function it_counts_last_page_of_slice_correctly()
+    {
+        $result = $this->createResultWithItems(11);
+
+        $page = $result->take(10, 10);
+
+        $this->assertEquals(2, $page->getCurrentPage());
+        $this->assertEquals(10, $page->getCurrentOffset());
+        $this->assertEquals(10, $page->getCurrentLimit());
+        $this->assertCount(1, $page);
+    }
+
     abstract protected function createResultWithItems($count);
 }
